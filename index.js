56073967781eb7f03a070ee2c77ed7fde64f0ca4
Kolonel1 <victor.munyao@student.moringaschool.com>
2022-08-28 01:15:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let array = word.split('').reverse().join('')
+  if (array ===word){
+    return true
+  }
+  else {
+    return false
+  }
+
+  
+  
 }
+
 
 /* 
   Add your pseudocode here
 */
 
-/*
-  Add written explanation of your solution here
+/*a palindrome means that the order of the string when reversed is the same as when normal
+there is no method that can reverse the order of a  string but there is one for arrays
+so you can split the string input into an array then reverse then join it again 
+
+
+add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
